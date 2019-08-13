a76aec2aa96e0516683f2d40be1f690a6474f760
Alex <alexr.daubricourt@gmail.com>
2019-08-13 15:58:10 +0200

    Added a network utility.

---
+++ Chresimos.Core/NetworkUtils.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Chresimos.Core
+{
+    public static class NetworkUtils
+    {
+        public static bool IsPortAvailable (int port)
+        {
+            var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var activeUdps = ipGlobalProperties.GetActiveUdpListeners();
+
+            return activeUdps.All(tcpi => tcpi.Port != port);
+        }
+    }
+}

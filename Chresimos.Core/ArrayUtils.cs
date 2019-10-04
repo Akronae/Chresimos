15e5561b616998b5bac379592d0b921129185f30
Akronae <alexr.daubricourt@gmail.com>
2019-10-04 15:43:03 +0200

    Array utils.

---
+++ Chresimos.Core/ArrayUtils.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Chresimos.Core
+{
+    public static class ArrayUtils
+    {
+        public static T[] GetRange <T> (this T[] data, int index, int length)
+        {
+            var result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            
+            return result;
+        }
+        
+    }
+}
